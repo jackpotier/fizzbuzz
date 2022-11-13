206b775879ab1c9d05e3ff8c478c78d6c2a1beb8
Jack Potier <jackpotierschool@gmail.com>
2022-11-14 09:40:42 +1300

    finished the little game :)

---
+++ index.js
@@ -0,0 +1,17 @@
+let input = prompt("Please enter an integer");
+input = Number(input)
+
+for (let i=1;i<=input;i++){
+    if (i%3==0 && i%5==0){
+        console.log("Fizzbuzz");
+    }
+    else if (i%3==0){
+        console.log("Fizz")
+    }
+    else if (i%5==0){
+        console.log("Buzz")
+    }
+    else {
+        console.log(i)
+    }
+}
